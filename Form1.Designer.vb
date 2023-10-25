aeeb6212e4fc6275b680742dda4a463349196d7a
alexandrudami <alexandrudami@gmail.com>
2023-10-25 11:39:31 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        dgv = New DataGridView()
+        ToolStrip1 = New ToolStrip()
+        btnADD = New ToolStripButton()
+        ToolStripButton1 = New ToolStripButton()
+        CType(dgv, ComponentModel.ISupportInitialize).BeginInit()
+        ToolStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' dgv
+        ' 
+        dgv.AllowUserToAddRows = False
+        dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgv.Dock = DockStyle.Fill
+        dgv.Location = New Point(0, 25)
+        dgv.Name = "dgv"
+        dgv.RowTemplate.Height = 25
+        dgv.Size = New Size(800, 425)
+        dgv.TabIndex = 0
+        ' 
+        ' ToolStrip1
+        ' 
+        ToolStrip1.Items.AddRange(New ToolStripItem() {btnADD, ToolStripButton1})
+        ToolStrip1.Location = New Point(0, 0)
+        ToolStrip1.Name = "ToolStrip1"
+        ToolStrip1.Size = New Size(800, 25)
+        ToolStrip1.TabIndex = 1
+        ToolStrip1.Text = "ToolStrip1"
+        ' 
+        ' btnADD
+        ' 
+        btnADD.DisplayStyle = ToolStripItemDisplayStyle.Image
+        btnADD.Image = CType(resources.GetObject("btnADD.Image"), Image)
+        btnADD.ImageTransparentColor = Color.Magenta
+        btnADD.Name = "btnADD"
+        btnADD.Size = New Size(23, 22)
+        btnADD.Text = "ToolStripButton1"
+        ' 
+        ' ToolStripButton1
+        ' 
+        ToolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image
+        ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), Image)
+        ToolStripButton1.ImageTransparentColor = Color.Magenta
+        ToolStripButton1.Name = "ToolStripButton1"
+        ToolStripButton1.Size = New Size(23, 22)
+        ToolStripButton1.Text = "ToolStripButton1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(dgv)
+        Controls.Add(ToolStrip1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dgv, ComponentModel.ISupportInitialize).EndInit()
+        ToolStrip1.ResumeLayout(False)
+        ToolStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents dgv As DataGridView
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents btnADD As ToolStripButton
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+End Class
---
+++ Form1.vb
@@ -0,0 +1,110 @@
+﻿
+
+
+Imports Microsoft.Data.SqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim cmmSelect As String = "Select PersID, Nume, Judet from Persoane Inner Join Judete ON Persoane.JudID = Judete.JudID"
+
+        Dim dt As New DataTable
+        Dim SQLCMM As New SQLCOMMANDS
+        SQLCMM.SQLSELECT(dt, cmmSelect)
+        dgv.DataSource = dt
+        dgv.Columns("PersID").Visible = False
+
+    End Sub
+
+    Private Sub btnADD_Click(sender As Object, e As EventArgs) Handles btnADD.Click
+        dgv.AllowUserToAddRows = True
+
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        Dim dt As New DataTable
+
+    End Sub
+
+    Private Sub dgv_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv.CellDoubleClick
+        If dgv.CurrentCell.ColumnIndex = 2 Then
+            Dim sqlcmm As New SQLCOMMANDS
+            Dim cmm As String = "Select * from Judete"
+            Dim frmList As New Form
+            Dim dgvList As New DataGridView
+
+            Dim dtList As New DataTable
+
+
+
+            frmList.Size = New Drawing.Size(500, 500)
+            dgvList.Dock = DockStyle.Fill
+
+            sqlcmm.SQLSELECT(dtList, cmm)
+
+            dgvList.DataSource = dtList
+            frmList.Controls.Add(dgvList)
+
+            frmList.Show()
+
+
+
+            AddHandler dgvList.CellDoubleClick, AddressOf dgvList_CellDoubleClick
+        End If
+    End Sub
+
+    Private Sub dgvList_CellDoubleClick()
+
+
+    End Sub
+
+
+End Class
+
+
+
+Public Class SQLCOMMANDS
+
+
+
+    Dim cnn As String = "Data Source=ALEX-PC\SQLEXPRESS01;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"
+    Dim adapter As SqlDataAdapter
+
+    Public Sub SQLCONN(ByRef conn As SqlConnection)
+
+        conn.ConnectionString = cnn
+
+    End Sub
+    Public Sub SQLSELECT(ByRef dt As DataTable, ByVal cmm As String)
+
+
+
+        adapter = New SqlDataAdapter
+        Dim CONN As New SqlConnection
+        SQLCONN(CONN)
+
+        Using CONN
+            CONN.Open()
+            adapter.SelectCommand = New SqlCommand(cmm, CONN)
+            adapter.Fill(dt)
+        End Using
+
+    End Sub
+
+    Public Sub SQLINSERT(ByRef dt As DataTable, ByVal loc As String, ByVal val As String)
+
+        Dim cmm As String = "Insert into " & loc & "values (" & val & ")"
+
+        adapter = New SqlDataAdapter
+        Dim conn As New SqlConnection
+        SQLCONN(conn)
+        Using conn
+            conn.Open()
+            adapter.InsertCommand = New SqlCommand(cmm, conn)
+            adapter.Fill(dt)
+        End Using
+
+
+
+    End Sub
+
+End Class
